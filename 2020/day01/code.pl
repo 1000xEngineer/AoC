ffa0215e08990353eac0d29cc39e6facc670fdb1
The 1000x Engineer <75281350+1000xEngineer@users.noreply.github.com>
2020-12-01 21:17:07 +0000

    Make the summer generic

--- 2020/day01/code.pl
+++ 2020/day01/code.pl
@@ -1,57 +1,56 @@
 
+% Run the day's program.
 start :-
     read_input(Input),
+    find_sums(Input, 2, 2020, [A, B]),
+    find_sums(Input, 3, 2020, [C, D, E]),
     !,
-    find_summing_entries(Input, A, B),
-    find_summing_entries2(Input, C, D, E),
     Result is A * B,
     Result2 is C * D * E,
     format("Two numbers: ~p ~p Result: ~p~n", [A, B, Result]),
     format("Three numbers: ~p ~p ~p Result: ~p~n", [C, D, E, Result2]).
 
+% Read the file into a string and unify Input to a list of numbers.
 read_input(Input) :-
     read_file_to_string("input.txt", String, []),
     split_string(String, "\n", "\n", Strings),
     read_input(Strings, [], Input).
 
+% Unify each string Num to a number Number and add it to Input.
 read_input([], Input, Input).
 read_input([Num|Rest], Numbers, Input) :-
     number_string(Number, Num),
     append(Numbers, [Number], Numbers1),
     read_input(Rest, Numbers1, Input).
 
-find_summing_entries([], _, _) :- !.
-find_summing_entries([First|Rest], A, B) :-
-    find_summing_entries(First, Rest, A, B),
-    find_summing_entries(Rest, A, B).
 
-find_summing_entries(_, [], _, _) :- !.
-find_summing_entries(First, [Second|Rest], A, B) :-
-    Result is First + Second,
-    Result = 2020,
-    !,
-    A = First,
-    B = Second
-    ;
-    find_summing_entries(First, Rest, A, B).
+% find_sums finds Count numbers with a sum Sum in List.
 
-find_summing_entries2([], _, _, _) :- !.
-find_summing_entries2([First|Rest], A, B, C) :-
-    find_summing_entries2(First, Rest, A, B, C),
-    find_summing_entries2(Rest, A, B, C).
+% Base condition, empty list = cut
+find_sums([], _, _, _) :- !.
+% Special case for Count = 1 where we just find the number.
+find_sums([First|_], 1, First, [First]).
+find_sums([_|Rest], 1, Sum, Outputs) :-
+    find_sums(Rest, 1, Sum, Outputs).
+% General case, recurse to find the other numbers.
+find_sums([Num|Rest], Count, Sum, Outputs) :-
+    % Check Count for sanity
+    Count >= 2,
 
-find_summing_entries2(_, [], _, _, _) :- !.
-find_summing_entries2(First, [Second|Rest], A, B, C) :-
-    find_summing_entries2(First, Second, Rest, A, B, C),
-    find_summing_entries2(First, Rest, A, B, C).
+    % Calculate remaining amount
+    Remainder is Sum - Num,
+    Count1 is Count - 1,
 
-find_summing_entries2(_, _, [], _, _, _) :- !.
-find_summing_entries2(First, Second, [Third|Rest], A, B, C) :-
-    Result is First + Second + Third,
-    Result = 2020,
-    !,
-    A = First,
-    B = Second,
-    C = Third
-    ;
-    find_summing_entries2(First, Second, Rest, A, B, C).
+    % Find the rest of the numbers
+    find_sums(Rest, Count1, Remainder, Out1),
+
+    sum_list(Out1, Sum1),
+    (
+        % If the list of the sum is the remainder, then we unify Outputs
+        % with the new number.
+        Sum1 = Remainder,
+        Outputs = [Num|Out1]
+        ;
+        % Otherwise, just continue on.
+        find_sums(Rest, Count, Sum, Outputs)
+    ).
